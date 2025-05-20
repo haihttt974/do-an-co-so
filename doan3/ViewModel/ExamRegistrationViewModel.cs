76b316c81796ee358bab40f8f139ab3e762ec0b4
Lê Duy Hải <leduyhai090704@gmail.com>
2025-05-20 22:19:34 +0700

    dang ky thi

---
+++ doan3/ViewModel/ExamRegistrationViewModel.cs
@@ -0,0 +1,18 @@
+﻿using doan3.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace doan3.ViewModel
+{
+    public class ExamRegistrationViewModel
+    {
+        public int UserId { get; set; }
+        public int HocVienId { get; set; }
+        public List<LichThi> AvailableExams { get; set; } = new List<LichThi>();
+        [Required(ErrorMessage = "Vui lòng chọn lịch thi")]
+        public int SelectedLichThiId { get; set; }
+        public string RegistrationMessage { get; set; }
+        public CtDangKyThi RegisteredExam { get; set; }
+    }
+}
